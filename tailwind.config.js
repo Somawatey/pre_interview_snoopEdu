d770992e361611ed30823ea4be76f5b7feeeef20
Somawatey <148378323+Somawatey@users.noreply.github.com>
2025-04-11 11:48:20 +0700

    first draft

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./**/*.html'], // Adjust the path as necessary
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
